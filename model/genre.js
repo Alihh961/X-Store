f75275251cfe792294b2dd26b19587a6c916896e
Alihh961 <hajhassan.ali@outlook.com>
2024-10-08 08:43:00 +0200

    editing genre model (set name and slug to unique)

--- model/genre.js
+++ model/genre.js
@@ -4,10 +4,12 @@ const genreSchema = new mongoose.Schema(
     {
         name:{
             type :String,
+            unique,
             required : [true , 'Genre name is required']
         },
         slug:{
             type :String,
+            unique,
             required : [true , 'Genre Slug is required']
         }
     }
