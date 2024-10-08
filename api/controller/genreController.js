e2fafed4d573c62c52b5ba031ac335b2c69247a9
Alihh961 <hajhassan.ali@outlook.com>
2024-10-08 09:38:57 +0200

    adding edit and delete method for genre model

--- api/controller/genreController.js
+++ api/controller/genreController.js
@@ -1,50 +1,140 @@
-const genreModel = require('../../model/genre');
+const genreModel = require("../../model/genre");
 
-const addGenre = async (req,res)=>{
+const addGenre = async (req, res) => {
+  const genreName = req.body.name;
+  const genreSlug = req.body.slug;
 
-    const genreName = req.body.name;
-    const genreSlug = req.body.slug;
+  if (!genreName) {
+    return res.status(400).json({
+      message: "Genre name is required",
+      status: "fail",
+    });
+  }
+  if (!genreSlug) {
+    return res.status(400).json({
+      message: "Genre slug is required",
+      status: "fail",
+    });
+  }
 
-    if(!genreName){
-        return res.status(400).json({
-            message : "Genre name is required",
-            status : "fail"
-        })
+  const genre = new genreModel({ name: genreName, slug: genreSlug });
+
+  try {
+    const newGenre = await genre.save();
+
+    return res.status(200).json({
+      message: "Genre created successfully",
+      data: {
+        newGenre,
+      },
+      status: "success",
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({
+        message: "Genre name or genre slug already exists",
+        status: "fail",
+      });
     }
-    if(!genreSlug){
-        return res.status(400).json({
-            message : "Genre slug is required",
-            status : "fail"
-        })
+    return res.status(400).json({
+      message: error.message,
+      status: "fail",
+    });
+  }
+};
+
+const editGenreNameOrSlugById = async (req, res) => {
+  const genreId = req.params.id;
+  const genreName = req.body.name;
+  const genreSlug = req.body.slug;
+
+  if (!genreId || !genreId.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(400).json({
+      message: "ID is not a valid MongoDB _id, Please Check ID",
+      status: "fail",
+    });
+  }
+
+  if (!genreName && !genreSlug) {
+    return res.status(400).json({
+      message: "No name or slug provided",
+      status: "fail",
+    });
+  }
+
+  try {
+    let updatedGenre;
+
+    if(genreName && genreSlug){
+        updatedGenre = await genreModel.findByIdAndUpdate( 
+            genreId,
+            {name : genreName , slug : genreSlug},
+            {new : true}
+        )
+    }else if(genreName){
+
+        updatedGenre = await genreModel.findByIdAndUpdate( 
+            genreId,
+            {name : genreName},
+            {new : true}
+        )
+    }else if(genreSlug){
+        updatedGenre = await genreModel.findByIdAndUpdate( 
+            genreId,
+            {slug : genreSlug},
+            {new : true}
+        )
     }
 
-    const genre = new genreModel({name : genreName , slug : genreSlug});
+    return res.status(200).json({
+        message : "Genre updated successfully",
+        data : {
+            genre : updatedGenre
+        },
+        status : "success"
+    })
 
-    try{
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({
+        message: "Genre name or genre slug already exists",
+        status: "fail",
+      });
+    }
 
-        const newGenre = await genre.save();
+    return res.status(400).json({
+      message: error.message,
+      status: "fail",
+    });
+  }
+};
+
+const deleteGenreById = async(req,res)=>{
+    const genreId = req.params.id;
+
+    if (!genreId || !genreId.match(/^[0-9a-fA-F]{24}$/)) {
+        return res.status(400).json({
+          message: "ID is not a valid MongoDB _id, Please Check ID",
+          status: "fail",
+        });
+      }
+
+      try{
+
+        await genreModel.findByIdAndDelete(genreId);
 
         return res.status(200).json({
-            message : "Genre created successfully",
-            data : {
-                newGenre
-            },
+            message : "Genre deleted successfully",
             status : "success"
         })
-        
-    }catch(error){
-        if(error.code === 11000){
-            return res.status(400).json({
-                message : "Genre name or genre slug already exists",
-                status : "fail"
-            })
-        }
+
+      }catch(error){
         return res.status(400).json({
             message : error.message,
-            status : "fail"
+            status : 'fail'
         })
-    }
+      }
+
 }
 
-
-module.exports = { addGenre}
+module.exports = { addGenre , editGenreNameOrSlugById ,deleteGenreById };
--- api/router/genreRouter.js
+++ api/router/genreRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const genreController = require('../controller/genreController')
 
 router.post('/' , genreController.addGenre);
+router.patch('/:id' , genreController.editGenreNameOrSlugById);
+router.delete('/:id', genreController.deleteGenreById);
 
 
 
